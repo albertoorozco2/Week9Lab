755bf99efedd3fe1ee9133a6d28c961b2fbe39a9
albertoorozco2 <albertoorozco2@hotmail.com>
2017-03-31 17:06:22 +0100

    grayscale filter add

--- src/FilterPictures.java
+++ src/FilterPictures.java
@@ -18,14 +18,14 @@ public class FilterPictures extends PApplet {
 //	    RedTintedPicture rtp = new RedTintedPicture(this);
 //		PImage redTintedPicture = rtp.filter(myPicture);
 //		// Apply a blue tint
-		BlueTintedPicture btp = new BlueTintedPicture(this);
-		PImage blueTintedPicture = btp.filter(myPicture);
+//		BlueTintedPicture btp = new BlueTintedPicture(this);
+//		PImage blueTintedPicture = btp.filter(myPicture);
 //		// Apply a green tint
 //		GreenTintedPicture gtp = new GreenTintedPicture(this);
 //		PImage greenTintedPicture = gtp.filter(myPicture);
-//		// Convert the image to greyscale
-//		GreyscaledPicture gsp = new GreyscaledPicture(this);
-//		PImage greyscaledPicture = gsp.filter(myPicture);
+		// Convert the image to greyscale
+		GreyscaledPicture gsp = new GreyscaledPicture(this);
+		PImage greyscaledPicture = gsp.filter(myPicture);
 //		// Threshold the image at a value of say 50
 //		// we will pass in the greyscaled picture for now
 //		ThresholdedPicture tp = new ThresholdedPicture(this);
